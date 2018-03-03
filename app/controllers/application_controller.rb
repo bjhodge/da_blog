b2920b857bd3476b698fe86ca6f6d3e6a75c4996
Bryan Hodge <bjhodge1212@gmail.com>
2018-03-03 12:27:31 -0500

    added image to post function, need to fix display image once submitted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar])
 
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar])
+
   end
 end
