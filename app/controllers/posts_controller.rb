5c82960b4dbe9734faac44524b3d15cc92d65468
Bryan Hodge <bjhodge1212@gmail.com>
2018-03-06 08:09:11 -0500

    Able to add image to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,10 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def user_posts
+    @user = User.find(params[:id])
+  end
+
   # GET /posts/1
   # GET /posts/1.json
   def show
